670714ec3d8990e8e5a6a2d656f5a5681b0cbeae
IsaacLaRose <ihlarose4444@gmail.com>
2025-11-13 10:01:33 -0500

    Updated TMDB to also return image link for movies within json
    Images are added to jason via path

--- backend/API/moviedb.js
+++ backend/API/moviedb.js
@@ -4,9 +4,14 @@ dotenv.config();
 const API_KEY = process.env.TMDBKEY;
 export async function searchMovie(query) {
   const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`;
-
   const response = await fetch(url);
   const data = await response.json();
 
-  return data.results;
+  return data.results.map(movie => ({
+    id: movie.id,
+    title: movie.title,
+    release_date: movie.release_date,
+    overview: movie.overview,
+    poster: movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : null //Want to give image too
+  }));
 }
